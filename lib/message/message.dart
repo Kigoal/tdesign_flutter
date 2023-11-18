3160af56e518241ab734477b6a3788d82face7f3
Kigoal <i@kigoal.cc>
2023-11-18 12:54:50 +0800

    🦄 refactor(message): 重构消息提示组件

--- lib/message/message.dart
+++ lib/message/message.dart
@@ -6,49 +6,48 @@ import 'package:tdesign_icons_flutter/tdesign_icons_flutter.dart';
 import '../theme/export.dart';
 import './interface.dart';
 
-const kTdMessageIconSize = 22.0;
-const kTdMeesageHeight = 48.0;
-const kTdMeesageSpacer = 8.0;
-
-const kTdMessageDefaultDuration = Duration(seconds: 3);
+const _kTdMessageIconSize = 22.0;
+const _kTdMeesageHeight = 48.0;
+const _kTdMeesageOffset = 12.0;
+const _kTdMessageDuration = Duration(seconds: 3);
 
 typedef TdMessageClose = void Function();
 
 class TdMessage extends StatefulWidget {
   const TdMessage({
     super.key,
-    this.type = TdMessageType.info,
-    required this.title,
-    this.icon,
-    this.textAlign = TextAlign.start,
-    this.duration = const Duration(seconds: 3),
     this.onClosing,
+    this.type = TdMessageType.info,
+    this.duration = _kTdMessageDuration,
+    this.textAlign = TextAlign.start,
+    this.icon,
+    required this.content,
   });
+
+  /// 结束时触发的事件
+  final TdMessageClose? onClosing;
 
   /// 消息组件风格
   final TdMessageType type;
 
-  /// 用于自定义消息弹出内容
-  final Text title;
-
-  /// 消息提醒前面的图标
-  final Icon? icon;
+  /// 持续时长
+  final Duration duration;
 
   /// 文本对齐方式
   final TextAlign textAlign;
 
-  /// 持续时长
-  final Duration duration;
+  /// 消息提醒前面的图标
+  final Icon? icon;
 
-  /// 结束时触发的事件
-  final TdMessageClose? onClosing;
+  /// 用于自定义消息弹出内容
+  final Text content;
 
   @override
   State<TdMessage> createState() => _TdMessageState();
 }
 
-class _TdMessageState extends State<TdMessage>
-    with SingleTickerProviderStateMixin {
+class _TdMessageState extends State<TdMessage> with SingleTickerProviderStateMixin {
+  /// 进入/退出动画
   late final AnimationController _animationController;
 
   @override
@@ -60,10 +59,10 @@ class _TdMessageState extends State<TdMessage>
       duration: kThemeAnimationDuration,
     );
 
-    /// 开始进入动画
+    // 开始进入动画
     _animationController.forward();
 
-    /// 定时器
+    // 定时器
     Timer(widget.duration, () async {
       /// 开始退出动画
       await _animationController.reverse();
@@ -82,29 +81,29 @@ class _TdMessageState extends State<TdMessage>
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-
     final theme = TdTheme.of(context);
 
-    final offset = mediaQuery.padding.top + kTdMeesageHeight;
+    final offset = MediaQuery.of(context).padding.top + _kTdMeesageOffset;
 
     return AnimatedBuilder(
       animation: _animationController,
-      child: Align(
-        alignment: Alignment.topCenter,
-        child: SafeArea(
-          minimum: EdgeInsets.all(theme.spacer2),
+      child: Padding(
+        padding:  EdgeInsets.symmetric(
+          horizontal: theme.spacer2,
+        ),
+        child: Align(
+          alignment: Alignment.topCenter,
           child: TdRawMessage(
-            type: widget.type,
-            title: widget.title,
-            icon: widget.icon,
-            textAlign: widget.textAlign,
-          ),
+              type: widget.type,
+              textAlign: widget.textAlign,
+              icon: widget.icon,
+              content: widget.content,
+            ),
         ),
       ),
       builder: (context, child) {
         return Positioned(
-          top: offset * (_animationController.value - 1.0),
+          top: offset + _kTdMeesageHeight * (_animationController.value - 1.0),
           left: 0.0,
           right: 0.0,
           child: child!,
@@ -117,38 +116,36 @@ class _TdMessageState extends State<TdMessage>
 class TdRawMessage extends StatelessWidget {
   const TdRawMessage({
     super.key,
-    required this.type,
-    required this.title,
+    this.type = TdMessageType.info,
+    this.textAlign = TextAlign.start,
     this.icon,
-    required this.textAlign,
+    required this.content,
   });
 
   /// 消息组件风格
   final TdMessageType type;
 
-  /// 用于自定义消息弹出内容
-  final Text title;
+  /// 文本对齐方式
+  final TextAlign textAlign;
 
   /// 消息提醒前面的图标
   final Icon? icon;
 
-  /// 文本对齐方式
-  final TextAlign textAlign;
+  /// 用于自定义消息弹出内容
+  final Text content;
 
   Color _buildThemeColor(TdThemeData theme) {
     return switch (type) {
-      TdMessageType.info => theme.brandColor6,
-      TdMessageType.success => theme.successColor6,
-      TdMessageType.warning => theme.warningColor6,
-      TdMessageType.error => theme.errorColor6,
+      TdMessageType.info => theme.brandColor,
+      TdMessageType.success => theme.successColor,
+      TdMessageType.warning => theme.warningColor,
+      TdMessageType.error => theme.errorColor,
     };
   }
 
   Icon _buildThemeIcon(TdThemeData theme) {
     return switch (type) {
-      TdMessageType.info ||
-      TdMessageType.warning =>
-        const Icon(TdIcons.info_circle_filled),
+      TdMessageType.info || TdMessageType.warning => const Icon(TdIcons.info_circle_filled),
       TdMessageType.success => const Icon(TdIcons.check_circle_filled),
       TdMessageType.error => const Icon(TdIcons.error_circle_filled),
     };
@@ -159,12 +156,12 @@ class TdRawMessage extends StatelessWidget {
     final theme = TdTheme.of(context);
 
     return Container(
-      height: kTdMeesageHeight,
       padding: EdgeInsets.symmetric(
+        vertical: theme.spacer1,
         horizontal: theme.spacer2,
       ),
       decoration: BoxDecoration(
-        color: theme.textColorAnti,
+        color: theme.backgroundColorContainer,
         borderRadius: theme.radiusDefault,
         boxShadow: theme.shadow4,
       ),
@@ -177,7 +174,7 @@ class TdRawMessage extends StatelessWidget {
             ),
             child: IconTheme(
               data: IconThemeData(
-                size: kTdMessageIconSize,
+                size: _kTdMessageIconSize,
                 color: _buildThemeColor(theme),
               ),
               child: icon ?? _buildThemeIcon(theme),
@@ -189,9 +186,9 @@ class TdRawMessage extends StatelessWidget {
                 color: theme.textColorPrimary,
               ),
               textAlign: textAlign,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 1,
-              child: title,
+              // overflow: TextOverflow.ellipsis,
+              // maxLines: 1,
+              child: content,
             ),
           ),
         ],
@@ -206,7 +203,7 @@ void showTdMessage(
   required Text title,
   Icon? icon,
   TextAlign textAlign = TextAlign.start,
-  Duration duration = kTdMessageDefaultDuration,
+  Duration duration = _kTdMessageDuration,
 }) {
   late final OverlayEntry overlayWidget;
 
@@ -220,7 +217,7 @@ void showTdMessage(
       return TdMessage(
         onClosing: handleClosing,
         type: type,
-        title: title,
+        content: title,
         icon: icon,
         textAlign: textAlign,
       );
@@ -281,4 +278,112 @@ void showTdErrorMessage(
     title: title,
     icon: icon,
   );
+}
+
+class TdMessagePlugin {
+  static OverlayEntry? _show(
+    BuildContext context, {
+    required TdMessageType type,
+    Duration? duration,
+    TextAlign? textAlign,
+    Icon? icon,
+    required Text content,
+  }) {
+    OverlayEntry? overlayWidget;
+
+    void handleClosing() {
+      /// 删除元素
+      overlayWidget?.remove();
+    }
+
+    overlayWidget = OverlayEntry(
+      builder: (context) {
+        return TdMessage(
+          onClosing: handleClosing,
+          type: type,
+          duration: duration ?? _kTdMessageDuration,
+          textAlign: textAlign ?? TextAlign.left,
+          content: content,
+          icon: icon,
+        );
+      },
+    );
+
+    /// 插入元素
+    Overlay.of(context).insert(overlayWidget);
+
+    return overlayWidget;
+  }
+
+  /// 信息
+  static OverlayEntry? info(
+    BuildContext context, {
+    Duration? duration,
+    TextAlign? textAlign,
+    Icon? icon,
+    required Text content,
+  }) {
+    return _show(
+      context,
+      type: TdMessageType.info,
+      duration: duration,
+      textAlign: textAlign,
+      icon: icon,
+      content: content,
+    );
+  }
+
+  /// 警告
+  static OverlayEntry? warning(
+    BuildContext context, {
+    Duration? duration,
+    TextAlign? textAlign,
+    Icon? icon,
+    required Text content,
+  }) {
+    return _show(
+      context,
+      type: TdMessageType.info,
+      duration: duration,
+      textAlign: textAlign,
+      icon: icon,
+      content: content,
+    );
+  }
+
+  /// 错误
+  static OverlayEntry? error(
+    BuildContext context, {
+    Duration? duration,
+    TextAlign? textAlign,
+    Icon? icon,
+    required Text content,
+  }) {
+    return _show(
+      context,
+      type: TdMessageType.info,
+      duration: duration,
+      textAlign: textAlign,
+      icon: icon,
+      content: content,
+    );
+  }
+
+  /// 成功
+  static OverlayEntry? success(
+    BuildContext context, {
+    Duration? duration,
+    TextAlign? textAlign,
+    Icon? icon,
+    required Text content,
+  }) {
+    return _show(
+      context,
+      type: TdMessageType.info,
+      duration: duration,
+      textAlign: textAlign,
+      icon: icon,
+      content: content,
+    );
+  }
 }
